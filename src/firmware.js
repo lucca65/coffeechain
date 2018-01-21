aaa28fb7e4e29d2f6aac841f5d2b6fe5303e7f60
Esdras Eduardo <esdrasedu@gmail.com>
2018-01-21 16:03:54 -0200

    add GPIO

--- src/firmware.js
+++ src/firmware.js
@@ -1,13 +1,15 @@
 console.log('Init firmware of CoffeeChain')
 const Web3 = require('web3')
 
-const provider = 'http://localhost:8545'
+const Gpio = require('onoff').Gpio
+
+const provider = process.env.PROVIDER ? process.env.PROVIDER : 'localhost'
 const contract = {
   address: '0x9f51bAbaa93F58A27FDabe5F4a77089C78aa21cf',
   abi: '[{"constant": false, "inputs": [], "name": "kill", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function"},{"constant": false, "inputs": [{"name": "addr", "type": "address"}], "name": "withdraw", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function"},{"constant": false, "inputs": [{"name": "_fee", "type": "uint256"}], "name": "setFee", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function"},{"constant": true, "inputs": [], "name": "owner", "outputs": [{"name": "", "type": "address"}], "payable": false, "stateMutability": "view", "type": "function"},{"constant": false, "inputs": [], "name": "buy", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function"},{"constant": true, "inputs": [], "name": "fee", "outputs": [{"name": "", "type": "uint256"}], "payable": false, "stateMutability": "view", "type": "function"},{"inputs": [{"name": "_fee", "type": "uint256"}], "payable": false, "stateMutability": "nonpayable", "type": "constructor"},{"anonymous": false, "inputs": [], "name": "OnCoffeeBought", "type": "event"}]'
 }
 
-const web3 = new Web3(new Web3.providers.HttpProvider(provider))
+const web3 = new Web3(new Web3.providers.HttpProvider('http://' + provider + ':8545'))
 
 if (web3.isConnected()) {
   console.log('Provider is connected')
@@ -29,6 +31,11 @@ coffeeChain.OnCoffeeBought().watch(function (error, result) {
     console.log('On Coffee Bought error:', error)
   } else {
     console.log('Make a coffee')
+    let led = new Gpio(16, 'out')
+    led.writeSync(1)
+    setTimeout(function () {
+      led.writeSync(0)
+    }, 10000)
   }
 })
 console.log('Start watch event OnCoffeeBought')
